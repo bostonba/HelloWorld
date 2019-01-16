03d5574cb73f91088eb732f986e1961970d28411
bostonba <46731357+bostonba@users.noreply.github.com>
2019-01-15 19:08:57 -0500

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,21 @@
+/**HelloPrinter
+ *Brianna Boston
+ *1/15/2019
+ *CMSC 255-002
+*/
+
+public class HelloPrinter{ 
+	public static void printHeading() {
+		System.out.println("Brianna Boston");
+		System.out.println("HelloPrinter");
+		System.out.println("Creating the first lab");
+		System.out.println("1/15/2019");
+		System.out.println("CMSC 255-002");
+		System.out.println();
+	}
+
+public static void main(String [] args) {
+	printHeading();
+    System.out.println("Hello");
+ 	}
+}
